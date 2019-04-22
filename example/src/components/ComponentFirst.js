7c2983f15df3532d53ef3e62d171a6ef53a350df
Oscar L <u1201508@unimilitar.edu.co>
2019-04-22 16:06:00 -0500

    video 38 tutorial react

---
+++ example/src/components/ComponentFirst.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class ComponentFirst extends React.Component {
+    render () {
+        return <p>Desde mi primer componente</p>
+    }
+}
+
+export default ComponentFirst;
