0a5b98958b91d201fa7f1f0b57812b1621620530
Oscar L <u1201508@unimilitar.edu.co>
2019-04-22 16:25:06 -0500

    Component Class or Conts

--- example/src/components/ComponentFirst.js
+++ example/src/components/ComponentFirst.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 class ComponentFirst extends Component {
     render () {
         return (
             <div>
+                <Header/>
                 <p>Desde mi primer componente</p>
+                <Footer/>
             </div>
         )
     }
---
+++ example/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () =>{
+        return (
+            <div>
+                <p>Componente const</p>
+            </div>
+        )
+    
+}
+
+export default Header;
